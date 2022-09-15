2f1ec1213fcff1c01150e17034516906bfd6b0bd
Thiago David <thiagodavidpd@gmail.com>
2022-09-15 19:48:26 -0300

    aula 4

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,25 +1,33 @@
-import { StatusBar } from 'react-native';
-import {useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black} from '@expo-google-fonts/inter'
-import { Background } from './src/components/Background';
-import {Home} from './src/screens/Home'
-import { Loading } from './src/components/Loading';
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from '@expo-google-fonts/inter'
+
+import { Routes } from './src/routes';
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
   });
 
-
   return (
-    <Background >
-      <StatusBar 
+    <Background>
+      <StatusBar
         barStyle="light-content"
-        backgroundColor='transparent'
+        backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Home/> : <Loading />}
 
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
-
